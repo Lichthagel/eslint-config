b4a527382c5985a7e29cbde7cc10b6dc490fc2f3
Lichthagel <hey@lichthagel.de>
2026-06-21 10:41:56 +0200

    fix(tailwindcss): correctly include recommended config

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -5,11 +5,13 @@ import type { FlatConfigItemStrict } from "../types";
 const tailwindcss = async (): Promise<FlatConfigItemStrict[]> => {
   const { default: pluginTailwind } = await import("eslint-plugin-tailwindcss");
 
+  const recommendedConfig = pluginTailwind.configs.recommended as Linter.Config;
+
   return [
-    ...((pluginTailwind.configs.recommended as Linter.Config[]).map((config) => ({
-      ...config,
-      name: config.name?.replaceAll(":", "/"),
-    }))),
+    ({
+      ...recommendedConfig,
+      name: recommendedConfig.name?.replaceAll(":", "/"),
+    }),
     {
       name: "lichthagel/tailwindcss",
       rules: {
