ab76a00fd08a5712d606f511683a3d29f6ea5b28
Lichthagel <hey@lichthagel.de>
2026-08-22 10:18:47 +0200

    feat(solid): support v2

--- src/configs/solid.ts
+++ src/configs/solid.ts
@@ -3,19 +3,24 @@ import type { Linter } from "eslint";
 import type { FlatConfigItemStrict } from "../types";
 
 type SolidOptions = {
+  version?: 2;
   withTypescript?: boolean;
 };
 
 const solid = async (options: SolidOptions = {}): Promise<FlatConfigItemStrict[]> => {
   const {
+    version,
     withTypescript = false,
   } = options;
 
-  const { default: configSolid } = await import("eslint-plugin-solid/configs/recommended") as unknown as { default: Linter.Config };
+  const configModule = version === 2
+    ? await import("eslint-plugin-solid/configs/v2")
+    : await import("eslint-plugin-solid/configs/recommended");
+  const { default: configSolid } = configModule as unknown as { default: Linter.Config };
 
   return [
     {
-      name: "solid/recommended",
+      name: version === 2 ? "solid/v2" : "solid/recommended",
       files: ["**/*.jsx", ...(withTypescript ? ["**/*.tsx"] : [])],
       ...configSolid,
     },
